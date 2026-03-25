db1b36ae2a19135622c581d6ddd1fabb6ccccc5b
sergrehsaharcuk-code <sergrehsaharcuk@gmail.com>
2026-03-25 21:52:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ BOT_TOKEN = "8258353048:AAGUC5iygvTHyDXOX7N_1DGCbX_oMFt7pe4"
 API_ID = 34227994
 API_HASH = "e41d1f3aa94f97f07e5d4e8126cfa35e"
 ADMIN_IDS = [1526536345]
-ACCOUNTS_PER_PROXY = 2
 
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ACCOUNTS_DIR = os.path.join(BASE_DIR, "accounts")
 SESSIONS_DIR = os.path.join(BASE_DIR, "sessions")
 
+# СОЗДАЁМ ПАПКИ
 os.makedirs(ACCOUNTS_DIR, exist_ok=True)
 os.makedirs(SESSIONS_DIR, exist_ok=True)
